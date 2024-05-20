14b153605fa27f14e9cf2f20311bc439a83456a6
Caroline Bates <caroline.portilla@gmail.com>
2024-05-20 17:19:07 +0800

    update wording

--- src/components/InfoComponent/InfoComponent.tsx
+++ src/components/InfoComponent/InfoComponent.tsx
@@ -1,7 +1,7 @@
 const InfoComponent = () => {
 
     return (
-        <><p className="noMargin alignLeft">Enter colour in hex, HSL or RGB formats as shown below or select from the colour picker. Remember to click enter to start search:</p><ul><li>#ff0000</li><li>rgb(172, 194, 217)</li><li>hsl(211, 37%, 76%)</li></ul></>
+        <><p className="noMargin alignLeft">Enter colour in hex, HSL or RGB formats as shown below or select from the colour picker. Remember to click enter in input to start search:</p><ul><li>#ff0000</li><li>rgb(172, 194, 217)</li><li>hsl(211, 37%, 76%)</li></ul></>
     );
 };
 
