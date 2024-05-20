aae0686245ef3856f938ae08ec80fb7024b336cf
Caroline Bates <caroline.portilla@gmail.com>
2024-05-20 17:03:08 +0800

    fix typo

--- src/components/InfoComponent/InfoComponent.tsx
+++ src/components/InfoComponent/InfoComponent.tsx
@@ -1,7 +1,7 @@
 const InfoComponent = () => {
 
     return (
-        <><p className="noMargin alignLeft">Enter colour in the following formats or select from colour picker. Remember to click enter to start search:</p><ul><li>hex: #ff0000</li><li>rgb: rgb(172, 194, 217)</li><li>hsl: hsl(211, 37%, 76%)</li></ul></>
+        <><p className="noMargin alignLeft">Enter colour in the following formats or select from the colour picker. Remember to click enter to start search:</p><ul><li>hex: #ff0000</li><li>rgb: rgb(172, 194, 217)</li><li>hsl: hsl(211, 37%, 76%)</li></ul></>
     );
 };
 
