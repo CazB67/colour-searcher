149edfa10930e90cdebe30f443dbdbe54f576bc5
Caroline Bates <caroline.portilla@gmail.com>
2024-05-20 19:14:37 +0800

    update css

--- src/components/InfoComponent/InfoComponent.tsx
+++ src/components/InfoComponent/InfoComponent.tsx
@@ -1,7 +1,7 @@
 const InfoComponent = () => {
 
     return (
-        <><p className="noMargin alignLeft">Enter colour in hex, HSL or RGB formats as shown below or select from the colour picker. Remember to <strong>click enter</strong> in input to start search:</p><ul><li>#ff0000</li><li>rgb(172, 194, 217)</li><li>hsl(211, 37%, 76%)</li></ul></>
+        <><p className="noMargin alignLeft">Enter colour in hex, HSL or RGB formats as shown below or select from the colour picker. </p><p className="noMargin alignLeft">Remember to <strong>click enter</strong> in input to start search and see results in the table below:</p><ul><li>#ff0000</li><li>rgb(172, 194, 217)</li><li>hsl(211, 37%, 76%)</li></ul></>
     );
 };
 
